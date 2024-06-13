8454d1ee11205d913f5901f4f1cffbfb27c649f1
IhnatVelykoivan <ihnat.velykoivan@gmail.com>
2024-06-13 12:11:24 +0300

    #

---
+++ main.py
@@ -0,0 +1,70 @@
+# Practic work
+
+# Hello = True
+# while Hello:
+#     user_input = input("Enter letter: ")
+#     if user_input == 'l':
+#         print("Hello World!")
+#         continue
+#     if user_input == 'd':
+#         print("You Doomed!")
+#         continue
+#     if user_input == 'o':
+#         Hello = False
+#         print("Oh, no!")
+#     else:
+#         print("Not found") #
+
+
+# user_count_hobby = int(input("Enter hobby number: "))
+#
+# i = 0
+# hobby = []
+# while i < user_count_hobby:
+#     text = "Enter hobby: " + str(i + 1) + ": "
+#     hobby.append(input(text))
+#
+#     i += 1
+#
+# print(hobby)
+
+
+
+# num = int(input("Enter num: "))
+# Happy = True
+# if num <= 5:
+#     print("I'm happy!")  # I'm Happy!
+# else:
+#     print("I'm don't happy!")
+#     correct = True
+#
+# if num == 11:
+#         print("Oh No! Doomed!")
+#         pass
+# if num == 4:
+#     print("\tNum is \n4")  # \t \n
+#     pass
+#
+# print("Bye!")
+
+
+
+# work = True
+# while work:
+#     user_input = input("Enter word STOP: ")
+#     if user_input =='STOP':
+#         work = False
+#
+# print("While loop is done")
+
+
+
+# user_count_hobby = int(input("Enter hobby number: "))
+
+# i = 0
+# hobby = []
+# while i < user_count_hobby:
+#     text = "Enter hobby: " + str(i + 1) + ": "
+#     hobby.append(input(text))
+#
+#     i += 1
